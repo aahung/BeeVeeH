86039641be13cdfca59421442d53119ef7fc78e6
_sam <3804518+Aahung@users.noreply.github.com>
2017-12-10 00:52:15 -0800

    Add python version variance to prebuilt lib

--- init-accelerated.sh
+++ init-accelerated.sh
@@ -1,3 +1,4 @@
 plat=`uname -sm | tr " " "-" | tr '[:upper:]' '[:lower:]'`
-wget "https://beeveeh.nestederror.com/prebuilt-lib/${plat}.tar.gz"
+py_version=`python3 --version | cut -d ' ' -f 2 | cut -b 1-3`
+wget "https://beeveeh.nestederror.com/prebuilt-lib/${plat}-py-${py_version}.tar.gz"
 tar zxf "${plat}.tar.gz"
