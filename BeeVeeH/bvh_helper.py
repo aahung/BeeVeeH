Fix a bug causing wrong world coordinates calculation for BVHNode

BeeVeeH/bvh_helper.py
@@ -57,17 +57,20 @@ class BVHNode(object):
         for child in self.children:
             child.load_frame(frame_data_array)
 
-    def apply_transformation(self, parent_tran_matrix=np.identity(4), parent_coordinates=np.zeros((3,1))):
-        self.coordinates = parent_coordinates + np.array([[self.offsets[0]],
-                                                          [self.offsets[1]],
-                                                          [self.offsets[2]]])
+    def apply_transformation(self, parent_tran_matrix=np.identity(4)):
+        self.coordinates = np.zeros((3,1))
+        local_translation = np.array([[1, 0, 0, self.offsets[0]],
+                                     [0, 1, 0, self.offsets[1]],
+                                     [0, 0, 1, self.offsets[2]],
+                                     [0, 0, 0, 1]])
         tran_matrix = np.identity(4)
-        for channel in self.channels[3:]:
-            tran_matrix = np.dot(channel.matrix(), tran_matrix)
-        tran_matrix = np.dot(parent_tran_matrix, tran_matrix)
-        for child in self.children:
-            child.apply_transformation(tran_matrix, self.coordinates)
+        tran_matrix = np.dot(tran_matrix, parent_tran_matrix)
+        tran_matrix = np.dot(tran_matrix, local_translation)
+        for channel in self.channels:
+            tran_matrix = np.dot(tran_matrix, channel.matrix())
         self.coordinates = np.dot(tran_matrix, np.append(self.coordinates, [[1]], axis=0))[:3]
+        for child in self.children:
+            child.apply_transformation(tran_matrix)
 
     def str(self, show_coordinates=False):
         s = 'Node({name}), offset({offset})\n'\

tests/test_bvh_helper.py
@@ -18,14 +18,20 @@ class TestCase():
             assert(abs(np.linalg.norm(child.offsets) - np.linalg.norm(node.coordinates - child.coordinates)) < 0.0001) 
             TestCase.check_bvh_node_distance_against_parent(child)
 
+    def check_bvh_node_above_ground(node):
+        for child in node.children:
+            assert(node.coordinates[1] >= 0) 
+            TestCase.check_bvh_node_above_ground(child)
+
     def test_bvh_load_frame(self):
         file_path = '%s/rotate_hip.bvh' % BVH_DIR
         self.root, self.frames, self.frame_time = BVH.load(file_path)
         assert(len(self.frames) == 15)
         assert(self.frame_time == 1.8333)
-        for frame in self.frames:
+        for frame in self.frames[0:1]:
             self.root.load_frame(frame)
             self.root.apply_transformation()
+            print(self.root.str(True))
             TestCase.check_bvh_node_distance_against_parent(self.root)
 
     def test_bvh_load_frame2(self):
@@ -37,3 +43,4 @@ class TestCase():
             self.root.load_frame(frame)
             self.root.apply_transformation()
             TestCase.check_bvh_node_distance_against_parent(self.root)
+            TestCase.check_bvh_node_above_ground(self.root)